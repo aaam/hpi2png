aa24554307d5b69bd3eefce91bdf4b4e2d6ab2aa
aaam <alex.a.mandl@gmail.com>
2015-01-14 21:52:52 +0000

    initial push

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+#Test PNG output
 ../bin/hpi2png.pl 000203_4335_1011_oslp.hpi 000203_4335_1011_oslp.png && echo "PNG TEST O.K." || echo "PNG TEST FAILED."
+
+#Test GIF output
 ../bin/hpi2png.pl 000203_4335_1011_oslp.hpi 000203_4335_1011_oslp.gif && echo "GIF TEST O.K." || echo "GIF TEST FAILED."
+
+#Test HPI output, file overwrite protection
 ../bin/hpi2png.pl 000203_4335_1011_oslp.hpi 000203_4335_1011_oslp.hpi && echo "HPI TEST FAILED." || echo  "HPI TEST O.K."
